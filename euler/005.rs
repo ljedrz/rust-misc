01ae5a7d0ef944c13ed19d8488fee9d87f05dd1c
ljedrz <ljedrz@gmail.com>
2015-10-04 20:48:54 +0200

    euler 5

---
+++ euler/005.rs
@@ -0,0 +1,20 @@
+// 2520 is the smallest number that can be divided by each of the numbers from
+// 1 to 10 without any remainder.
+// What is the smallest positive number that is evenly divisible by all of the
+// numbers from 1 to 20?
+
+fn is_divisible(number: u64) -> bool {
+	for n in (1..21) {
+		if number % n != 0 { return false; }
+	}
+	true
+}
+
+fn main() {	
+	for n in (1u64..) {
+		if is_divisible(n) {
+			println!("{}", n);
+			return;
+		}
+	}
+}

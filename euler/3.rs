93d8215bb3dcf1aae5ab80b85914d73be2517d01
ljedrz <ljedrz@gmail.com>
2015-10-04 18:41:27 +0200

    euler 3

---
+++ euler/3.rs
@@ -0,0 +1,24 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+fn factors(number: u64) -> Vec<u64> {
+	let mut n = 1;
+	let mut factors = Vec::new();
+	
+	while n * n <= number {
+		if number % n == 0 { factors.push(n) }
+		n += 1;
+	}
+	
+	factors.push(number);
+	
+	factors
+}
+
+fn is_prime(number: u64) -> bool {
+	factors(number).len() == 2
+}
+
+fn main() {
+	println!("{}", factors(600851475143).iter().filter(|&f| is_prime(*f)).last().unwrap());
+}

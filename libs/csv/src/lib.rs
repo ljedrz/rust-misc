6abbf14cfa1e6bb19cb9f3b6b5cdf42f6a50e3f0
ljedrz <ljedrz@gmail.com>
2015-10-05 11:58:25 +0200

    add csv vectorization to the csv lib

--- libs/csv/src/lib.rs
+++ libs/csv/src/lib.rs
@@ -1,3 +1,16 @@
+#![allow(dead_code)]
+
+fn vectorize_csv(csv: &str, delim: char) -> Vec<Vec<String>> {
+	let csv_lines: Vec<&str> = csv.lines_any().collect();
+	let mut csv_vec = Vec::with_capacity(csv_lines.len());
+
+	for line in csv_lines {
+		csv_vec.push(line.split(|c| c == delim).map(|s| s.to_owned()).collect());
+	}
+
+	csv_vec
+}
+
 fn csv_to_xml(input: &str, delim: char, root_name: &str, row_name: &str, col_name: &str) -> String {
 	let mut output = String::new();
 
@@ -26,13 +39,13 @@ impl XmlStr for String {
 		self.push_str(elem_name);
 		self.push('>');
 	}
-	
+
 	fn close_elem(&mut self, elem_name: &str) {
 		self.push_str("</");
 		self.push_str(elem_name);
 		self.push('>');
 	}
-	
+
 	fn full_elem(&mut self, elem_name: &str, elem_content: &str) {
 		self.open_elem(elem_name);
 		self.push_str(elem_content);
@@ -42,7 +55,15 @@ impl XmlStr for String {
 
 #[cfg(test)]
 mod tests {
-    use super::csv_to_xml;
+    use super::{vectorize_csv, csv_to_xml};
+
+	#[test]
+	fn csv_vectorization_test() {
+		let vec1 = vec![vec!["herp", "derp", "durr"], vec!["1", "2", ""], vec!["", "b", "c"]];
+		let vec2 = vectorize_csv("herp;derp;durr\n1;2;\r\n;b;c", ';');
+
+		assert_eq!(vec1, vec2);
+	}
 
     #[test]
     fn csv_to_xml_test() {
